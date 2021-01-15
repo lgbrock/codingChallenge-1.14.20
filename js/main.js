feb505c21b1177b64f525906b8f80d173ecae2f3
Logan Brock <36110596+lgbrock@users.noreply.github.com>
2021-01-15 11:51:07 -0600

    JS working

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 // Please create a function that takes in the name of a person. This function should add this person to a names list and let them know their position on the list. The names list should be created outside of the function.//
 
+let nameList = []
 
+function addToArray(userName) {
+  nameList.push('userName');
+  console.log(`Welome ${userName}, you are at the ${nameList.length} position`)
+}
+
+addToArray('Turkey Dog', 'Logan');
 
 
 /*
